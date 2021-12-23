49f063c0beddf0a03786e4a2ebb9046d402beda6
JorgeSanchez7 <72813814+JorgeSanchez7@users.noreply.github.com>
2021-12-23 21:12:53 +0100

    Add files via upload

---
+++ proyectos/TdeJorge/TdeJorge/Window2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TdeJorge
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        MainWindow padre;
+
+        public Window2(MainWindow padre)
+        {
+            InitializeComponent();
+            this.padre = padre;
+        }
+
+        private void clickEmpezar(object sender, RoutedEventArgs e)
+        {
+            padre.setNombre(this.txtJugador.Text);
+            this.Close();
+        }
+    }
+}
